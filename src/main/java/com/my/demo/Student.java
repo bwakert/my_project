9ed756c1e6cb07333e3cece6123a1d104c1ee93c
bwa <ssi-biewa@dfmc.com.cn>
2022-11-17 22:41:44 +0800

    配置忽略文件

---
+++ src/main/java/com/my/demo/Student.java
@@ -0,0 +1,22 @@
+package com.my.demo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 地址
+     */
+    private String address;
+}
